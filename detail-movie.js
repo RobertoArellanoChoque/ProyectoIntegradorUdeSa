bd9dd961edf9531d715298f288192189ff60af5f
martiniano18 <martinianosanchez@udesa.edu.ar>
2021-11-12 08:39:53 -0300

    arganizando archivos y carpetas

---
+++ detail-movie.js
@@ -0,0 +1,55 @@
+let qs = location.search;
+let qsToObject = new URLSearchParams(qs);
+let id = qsToObject.get('id')
+console.log(id);
+
+let api_key = 'e200abfaa90988d674f8605d11e8b9d0'
+let url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key} ` 
+fetch(url)
+.then(function(response){
+    return response.json()
+
+})
+.then (function(data){
+    console.log(data);
+    let img = document.querySelector('img');
+    img.src = data.data.images.original.url
+
+
+    let favorito = []
+
+    let linkfav = document.querySelector('a');
+
+    linkfav.addEventListener ('click', function (event){
+        event.preventDefault();
+        favorito.push(id);
+    
+        let favoritoAString = JSON.stringify(favorito);
+        localStorage.setItem('favoritos', favoritoAString);
+
+
+    })
+
+})
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    
