8fa1dfda0f3697cea603557b95bbc25ef2306deb
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2021-11-19 10:48:58 -0400

    Arreglos en js

--- js/index.js
+++ js/index.js
@@ -1,31 +1,30 @@
+let queryString = location.search
+let qsToObject = new URLSearchParams(queryString)
+let id = qsToObject.get('id')
 
-console.log("Hola mundo")
 
-let url = 'https://api.themoviedb.org/3/tv/popular?api_key=cd97ce54561a25ea3bbdfae70457a394'
+let url = 'https://api.themoviedb.org/3/movie/popular?api_key=cd97ce54561a25ea3bbdfae70457a394'
 
 fetch(url)
-.then(function(response){
-    return response.json()
-})
-.then(function(data) {
-    console.log(data.results);
-    let info = data.results
-    let prueba = document.querySelector('prueba')
-    let pruebaLista = '';
-
-    for(let i=0; i<info ; i++ ){
-        pruebaLista += `<article>
-                            <img src=${info[i].poster_path}>  
-                            <p> Nombre:${info[i].name} </P>
-
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data.results);
+        let info = data.results
+        let prueba = document.querySelector('#prueba')
+        let pruebaLista = '';
+        for (let i = 0; i < 5; i++) {
+            pruebaLista += `<article>
+                          <img class="imagen" src="${info[i].poster_path}>
+                          <h3> Nombre:${info[i].title} </h3>
                         </article>`
-    }
-    
-})
-.catch(function(error){
-    console.log(error);
-    
-})
+        }
+        
+    })
+    .catch(function (error) {
+        console.log(error);
 
+    })
 
 
