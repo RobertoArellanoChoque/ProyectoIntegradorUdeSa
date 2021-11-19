7a0e8ee0edcb26bb305845da8a621d2b6e44fabe
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2021-11-19 11:41:04 -0400

    Arreglos al js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-let queryString = location.search
-let qsToObject = new URLSearchParams(queryString)
-let id = qsToObject.get('id')
-
 
 let url = 'https://api.themoviedb.org/3/movie/popular?api_key=cd97ce54561a25ea3bbdfae70457a394'
 
@@ -14,13 +10,16 @@ fetch(url)
         let info = data.results
         let prueba = document.querySelector('#prueba')
         let pruebaLista = '';
-        for (let i = 0; i < 5; i++) {
+        for (let i=0; i<5; i++ ){
             pruebaLista += `<article>
-                          <img class="imagen" src="${info[i].poster_path}>
-                          <h3> Nombre:${info[i].title} </h3>
-                        </article>`
+                               <img class="imagen" src="${info[i].poster_path}">
+                               <h3 class=tituoloprincipaldepelicula>
+                               <p class="titulopelicula"> Pelicula: ${info[i].original_title}</p>
+                               </h3>
+                               <p class="titulopelicula" > Estreno: ${info[i].release_date}</p>
+                            </article>`
         }
-        
+        prueba.innerHTML = pruebaLista;
     })
     .catch(function (error) {
         console.log(error);
