8e406eabe1479794373012e5bb2f3d608db23d59
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2021-11-18 22:51:20 -0400

    haciendo js del index

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 
+console.log("Hola mundo")
 
-fetch('https://api.themoviedb.org/3/tv/popular?api_key=cd97ce54561a25ea3bbdfae70457a394')
+let url = 'https://api.themoviedb.org/3/tv/popular?api_key=cd97ce54561a25ea3bbdfae70457a394'
 
+fetch(url)
+.then(function(response){
+    return response.json()
+})
+.then(function(data) {
+    console.log(data.results);
+    let info = data.results
+    let prueba = document.querySelector('prueba')
+    let pruebaLista = '';
 
+    for(let i=0; i<info ; i++ ){
+        pruebaLista += `<article>
+                            <img src=${info[i].poster_path}>  
+                            <p> Nombre:${info[i].name} </P>
 
-let pelicula = document.querySelectorAll("article")
-
-console.log(pelicula)
-
-for (let i=0; i<pelicula.length; ++i ){
+                        </article>`
+    }
     
-}
+})
+.catch(function(error){
+    console.log(error);
+    
+})
+
+
+
