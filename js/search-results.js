471c8433e7c9b232793590b7befff320191dcb1c
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2021-11-22 12:46:56 -0400

    Arreglos a la pagina de resultados

--- js/search-results.js
+++ js/search-results.js
@@ -2,7 +2,7 @@ console.log('Hola Mundo')
 
 let qs = location.search;
 let qsToObject = new URLSearchParams(qs);
-let palabra = qsToObject.get('buscar') //580489
+let palabra = qsToObject.get('buscar')
 console.log(palabra);
 
 let key = 'cd97ce54561a25ea3bbdfae70457a394';
@@ -14,4 +14,23 @@ fetch(url)
     })
     .then(function(data){
         console.log(data);
+        let info = data.results
+        let resultado = document.querySelector('#sectionBuscador')
+        let resultadoNuevo = '';
+        for(let i = 0; i < 3; i++){
+            resultadoNuevo +=`<article>
+                                 <img class="imagen" src="https://image.tmdb.org/t/p/w342${info[i].poster_path}" alt="${info[i].title}" >
+                                 <h3 class=tituoloprincipaldepelicula>
+                                 <p class="titulopelicula" >${info[i].original_title}</p>
+                                </h3>
+                                <p class="titulopelicula" > Estreno:${info[i].release_date}</p>
+                                </article>`
+
+        }
+        resultado.innerHTML = resultadoNuevo;
+
+    })
+    .catch(function (error) {
+        console.log(error);
+
     })
