6d8edddcee64d2e107d68fce8eef1ee5dd36eaed
martiniano18 <martinianosanchez@udesa.edu.ar>
2021-11-22 23:26:52 -0300

    finishing proyect

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -1,16 +1,16 @@
 let qs = location.search;
 let qsToObject = new URLSearchParams(qs);
-let id = qsToObject.get('id') //acceso a la propiedad id del objeto
+let id = qsToObject.get('id') //convierto el qs a obj literal y capturo el id
 console.log(id);
 
 let api_key = 'e200abfaa90988d674f8605d11e8b9d0';
 let url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
 
-fetch(url)
+fetch(url) 
     .then(function (response) {
         return response.json()
     })
-    .then(function (data) {
+    .then(function (data) { 
         console.log(data);
 
         let imagen = 'https://image.tmdb.org/t/p/w342'// selector
@@ -23,7 +23,7 @@ fetch(url)
         let duracion = document.querySelector('.duracion')
         let botonFavoritos = document.querySelector('.botonFavoritos')
         let estreno = document.querySelector('.estreno')
-        
+        //capturo el dom via clases y id
     
         
         img.src = imagen + data.poster_path;
@@ -34,47 +34,40 @@ fetch(url)
         genero.innerHTML += `<a href="genres.html?id=${data.genres[0].id}">${data.genres[0].name}</a>`;
         informacion.innerHTML += data.overview;
         titulosdetalles.innerHTML += data.title;
+        //inserto contenido a los elementos del dom capturados
     
         let favoritos = []
-        let recuperoStorage = localStorage.getItem('favoritoPeliculas');
+        let recuperoStorage = localStorage.getItem('favoritoPeliculas'); // traigo ,si es que hay algo en el storage
         
         console.log(recuperoStorage)
-        if (recuperoStorage != null) {
-            favoritos = JSON.parse(recuperoStorage);
+        if (recuperoStorage != null) { // != diferente
+            favoritos = JSON.parse(recuperoStorage); //recuperoStorage viene en JSON y uso parse para descomprimirlo en un array y asi poder manipularlo
 
         }
-        let letrasfavoritos = document.querySelector('.botonFavoritos');
-        if(favoritos.includes(id)){
+        
+        if(favoritos.includes(id)){ // si el array de favoritos tiene un id alguien agrego a favoritos algo y tiene que cambiar a quitar...
             botonFavoritos.innerText = 'QUITAR DE FAVORITOS'
         }
         else{
-            botonFavoritos.innerText = 'AÑADIR A FAVORITOS❤️'
+            botonFavoritos.innerText = 'AÑADIR A FAVORITOS❤️' // inverso a lo de arriba
 
         }
        
         
-        
-
-
-
-         
-
-        
-
-        botonFavoritos.addEventListener('click', function (event) {
-            event.preventDefault();
+       botonFavoritos.addEventListener('click', function (event) {
+            event.preventDefault(); // prevent default saca la propiedad de link al texto del boton
            
 
-            if (favoritos.includes(id)){
-                let idASacar = favoritos.indexOf(id);
-                favoritos.splice(idASacar, 1);
+            if (favoritos.includes(id)){ // si apreto quitar de favoritos , quiero que me saque el id del array y que cambie el texto
+                let idASacar = favoritos.indexOf(id); //capturo el id que quiero sacar
+                favoritos.splice(idASacar, 1); // con la propiedad splice quito el id del array
                 botonFavoritos.innerText = 'AÑADIR A FAVORITOS❤️'
             } else{
-                favoritos.push(id);
+                favoritos.push(id); // si no esta el id nunca fue agregado por ende apreto añadir a favoritos , pushear el id y cambiar el texto
              botonFavoritos.innerText = 'QUITAR DE FAVORITOS'
 
             }
-             let favoritoAString = JSON.stringify(favoritos);
+             let favoritoAString = JSON.stringify(favoritos); // con el JSON.stringify convierto array en json para guardar en storage devuelta
             localStorage.setItem('favoritoPeliculas', favoritoAString);
             console.log(localStorage)
 
@@ -84,7 +77,7 @@ fetch(url)
         
     })
 
-    .catch(function (e) {
+    .catch(function (e) { // catch intercepta error y lo muestra en consola
         console.log(e);
     })
 
--- js/lista-favoritos.js
+++ js/lista-favoritos.js
@@ -1,19 +1,19 @@
 
-let recuperoStoragePeliculas = localStorage.getItem('favoritoPeliculas');
+let recuperoStoragePeliculas = localStorage.getItem('favoritoPeliculas'); // agarro el id pusheado porque traigo lo que esta en el storage 
 let recuperoStorageSeries = localStorage.getItem('favoritoSeries');
-let favoritosPeliculas = JSON.parse(recuperoStoragePeliculas);
+let favoritosPeliculas = JSON.parse(recuperoStoragePeliculas); //lo convierto en algo manipulable
 let favoritosSeries = JSON.parse(recuperoStorageSeries);
 
 const imagen = 'https://image.tmdb.org/t/p/w342'
-let lista = document.querySelector('.peliculasFavoritas');
+let lista = document.querySelector('.peliculasFavoritas'); //capturo el dom de favoritos
 let listaSeries = document.querySelector('.seriesFavoritas');
-let contenidoLista = '';
+let contenidoLista = ''; // creo una lista vacia
 
-if (favoritosPeliculas == null&&favoritosSeries == null){
+if (favoritosPeliculas == null&&favoritosSeries == null){ //si no hay contenido en favoritos que aparzca el texto correspondiente
     lista.innerHTML = '<h2>No hay contenido seleccionado</h2>';
 }
 
-for (let i= 0; i<favoritosPeliculas.length; i++) {
+for (let i= 0; i<favoritosPeliculas.length; i++) {   // recorre el largo del array uno por uno
     let api_key = 'e200abfaa90988d674f8605d11e8b9d0';
     let url = ` https://api.themoviedb.org/3/movie/${favoritosPeliculas[i]}?api_key=${api_key}`;
 
@@ -24,16 +24,19 @@ for (let i= 0; i<favoritosPeliculas.length; i++) {
  })
 .then(function(data){
     console.log(data);
-    contenidoLista = 
+    contenidoLista = // elementos a insetar en contenidoLista
     
-    `  <article>
+    ` 
+    <a class="peliculasFavoritas" href="detail-movie.html?id=${data.id}">
+    <article>
         <h2>${data.title}</h2>
         <img src=${imagen+data.poster_path}>
-        <a href="">Ver contenido</a>
-        </article>`
+       
+        </article>
+        </a>`
        console.log(data) 
 
-    lista.innerHTML += contenidoLista;
+    lista.innerHTML += contenidoLista; // inserta todo 
 })
 .catch(function(error){
     console.log (error);
@@ -54,11 +57,14 @@ for (let i= 0; i<favoritosSeries.length; i++) {
    
     contenidoLista = 
     
-    `  <article>
+    `  
+    <a class="peliculasFavoritas" href="detail-series.html?id=${data.id}">
+    <article>
         <h2>${data.name}</h2>
         <img src=${imagen+data.poster_path}>
-        <a href="">Ver contenido</a>
-        </article>`
+        </article>
+    </a>
+    `
       
 
         listaSeries.innerHTML += contenidoLista;
